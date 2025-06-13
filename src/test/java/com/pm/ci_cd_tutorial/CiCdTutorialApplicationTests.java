5c8d6a6e07a30fd92f916be9fcb1ab4dc934d019
Joseph Aidoo <joseph.aidoo@rancard.com>
2025-06-13 12:44:57 +0000

    my codebase

---
+++ src/test/java/com/pm/ci_cd_tutorial/CiCdTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.ci_cd_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
